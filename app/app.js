74a879492d941fd71749f5100ec62f48e3f8b693
Kaidanov <2bhere4u@gmail.com>
2016-06-21 09:32:42 +0300

    refactoring to a better project structure renaming the templates cleaning out the comments

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@
      angular.module('myApp', [
       'ngRoute',
       'ngResource',
-      'myApp.view1',
+      'myApp.dashboard',
       'myApp.view2',
       'myApp.version',
       'app.core'
@@ -14,89 +14,7 @@
     config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
         $locationProvider.hashPrefix('!');
 
-        $routeProvider.otherwise({ redirectTo: '/view1' });
+        $routeProvider.otherwise({ redirectTo: '/dashboard' });
     }]);
 
 })();
-
-  //$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-  //$httpProvider.defaults.transformRequest = [function (data) {
-  //    var param = function (obj) {
-  //        var query = '';
-  //        var name, value, fullSubName, subName, subValue, innerObj, i;
-  //        for (name in obj) {
-  //            value = obj[name];
-  //            if (value instanceof Array) {
-  //                for (i = 0; i < value.length; ++i) {
-  //                    subValue = value[i];
-  //                    fullSubName = name + '[' + i + ']';
-  //                    innerObj = {};
-  //                    innerObj[fullSubName] = subValue;
-  //                    query += param(innerObj) + '&';
-  //                }
-  //            }
-  //            else if (value instanceof Object) {
-  //                for (subName in value) {
-  //                    subValue = value[subName];
-  //                    fullSubName = name + '[' + subName + ']';
-  //                    innerObj = {};
-  //                    innerObj[fullSubName] = subValue;
-  //                    query += param(innerObj) + '&';
-  //                }
-  //            }
-  //            else if (value !== undefined && value !== null) {
-  //                query = encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
-  //            }
-  //        }
-
-  //        return query.length ? query.substr(0, query.length - 1) : query;
-  //    };
-
-  //    return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
-  //}];
-//}]);
-
-
-//(function () {
-//    angular.module('myApp')
-//      .factory('dataservice', ['$http', function ($http) {
-//          var service = {
-//              getProducts: getProducts
-//          };
-
-//          return service;
-
-//          function getProducts() {
-//              return $http.get('data.json').then(getProductsComplete);
-//          }
-
-//          function getProductsComplete(data, status, headers, config) {
-//              return data.data;
-//          };
-//      }]);
-//}());
-
-//angular.module('myApp').factory('dataservice', ['$http', function ($http) {
-
-//           var service = {
-//               getProducts: getProducts
-//           };
- 
-//           return service;
-
-//           function getProducts() {
-//               return $http.get('data.json').then(getProductsComplete);
-//           }
-
-//           function getProductsComplete(data, status, headers, config) {
-//               return data.data;
-//           };
-
-           
-
-//}]);
-
-
-
-//app.run(['$q', '$rootScope', function ($q, $rootScope) {
-//}]);
--- app/core/dataservice.js
+++ app/core/dataservice.js
@@ -26,21 +26,3 @@
 })();
 
 
-//(function () {
-//    angular.module('myApp')
-//      .factory('dataservice', ['$http', function ($http) {
-//          var service = {
-//              getProducts: getProducts
-//          };
-
-//          return service;
-
-//          function getProducts() {
-//              return $http.get('data.json').then(getProductsComplete);
-//          }
-
-//          function getProductsComplete(data, status, headers, config) {
-//              return data.data;
-//          };
-//      }]);
-//}());
